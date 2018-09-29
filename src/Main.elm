9a124d54676d9eef65a96e007e84d9b153412b0f
Stephane Larocque <stephl001@gmail.com>
2018-09-29 11:14:52 -0400

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,56 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
+import Browser
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes exposing (src)
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Minesweeper" ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
